55a5b94756478ba2eae5bd59263f6fef7855d763
Arjob Mukherjee <mukherjeearjob@gmail.com>
2019-10-21 01:45:18 +0530

    Initial Commit. Bootloader, Despatcher, Guru ready, VFS (Not final version for Sugarcube)

--- src/prototype/devfs.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _H_DEVFS
-#define _H_DEVFS
-
-#include "fs.h"
-
-void register_devfs(device_t device, struct file_operations *fo, char
-					*device_name);
-#endif // _H_DEVFS
--- src/prototype/floppy.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef _H_FLOPPY_
-#define _H_FLOPPY_
-
-	void out(int port, int value);
-	int in(int port);
-#endif // _H_FLOPPY_
